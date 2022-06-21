4a3868c8e8687368e4fef0a309409f7f80cad06a
0xBahamoot <lam@incognito.org>
2022-06-21 13:20:24 +0700

    update fullnode url

--- app/constants/constants.tsx
+++ app/constants/constants.tsx
@@ -7,5 +7,5 @@ export const MainnetConfig = {
   ExplorerAPI: "https://api-explorer.incognito.org/api/v1/",
   AnalyticsEndpoint: "https://api-explorer.incognito.org/",
   CoinserviceEndpoint: "https://api-coinservice.incognito.org",
-  FullnodeEndpoint: "https://community-fullnode.incognito.org/fullnode",
+  FullnodeEndpoint: "https://fullnode.incognito.org/fullnode",
 };
